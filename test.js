ce9ae6284b5b5c2a714811b1e57f8c3fd1c9c535
Kyle Mitchell <kyle@kemitchell.com>
2015-12-27 12:59:26 -0800

    Test against SPDX list

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// Check that each string in the exported array is an SPDX license expression.
+
 var assert = require('assert')
 var parse = require('spdx-expression-parse')
 var osi = require('./')
@@ -7,3 +9,30 @@ osi
     assert.doesNotThrow(
       function() { parse(identifier) },
       ( identifier + ' is a valid SPDX identifier' )) })
+
+// Check that every SPDX identifier marked as OSI-approved by SPDX is included
+// in the exported array.
+
+var https = require('https')
+
+var spdx = 'https://spdx.org/licenses/licenses.json'
+var buffers = [ ]
+
+https.get(spdx, function(response) {
+  response
+    .on('data', function(buffer) {
+      buffers.push(buffer) })
+    .on('end', function() {
+      assert.deepEqual(
+        JSON.parse(Buffer.concat(buffers))
+          .licenses
+          .filter(function(license) { return license.isOsiApproved })
+          .map(function(license) { return license.licenseId })
+          .reduce(
+            function(missing, id) {
+              return (
+                ( osi.indexOf(id) >= 0 ) ?
+                  missing :
+                  missing.concat(id) ) },
+            [ ]),
+        [ ]) }) })
