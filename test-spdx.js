b1e14f265ce73dc611c322d32cddc331d2ad2e95
Kyle Mitchell <kyle@kemitchell.com>
2015-12-27 12:32:36 -0800

    Fix SPDX License List comparison test

--- test-spdx.js
+++ test-spdx.js
@@ -1,19 +1,25 @@
 var assert = require('assert')
 var osi = require('./')
 var https = require('https')
+
 var spdx = 'https://spdx.org/licenses/licenses.json'
+var buffers = [ ]
 
 https.get(spdx, function(response) {
-  var json = ''
   response
     .on('data', function(buffer) {
-      json += buffer.toString() })
+      buffers.push(buffer) })
     .on('end', function() {
-      JSON.parse(json)
-        .licenses
-        .filter(function(license) {
-          return license.isOsiApproved })
-        .forEach(function(listedLicense) {
-          assert(
-            ( osi.indexOf(listedLicense) >= 0 ),
-            listedLicense.licenseId + ' is listed as OSI approved, but not in list') })}) })
+      assert.deepEqual(
+        JSON.parse(Buffer.concat(buffers))
+          .licenses
+          .filter(function(license) { return license.isOsiApproved })
+          .map(function(license) { return license.licenseId })
+          .reduce(
+            function(missing, id) {
+              return (
+                ( osi.indexOf(id) >= 0 ) ?
+                  missing :
+                  missing.concat(id) ) },
+            [ ]),
+        [ ]) }) })
