5ce8f2639e3de3d206bd41d9eb8496a8b9e2015f
Kyle E. Mitchell <kyle@kemitchell.com>
2019-03-23 10:54:42 -0700

    Build list from spdx.org JSON

---
+++ build.js
@@ -0,0 +1,27 @@
+var https = require('https')
+var URL = 'https://spdx.org/licenses/licenses.json'
+
+https.get(URL, function (response) {
+  var chunks = []
+  response
+    .on('data', function (chunk) {
+      chunks.push(chunk)
+    })
+    .once('error', function (error) {
+      console.error(error)
+      process.exit(1)
+    })
+    .once('end', function () {
+      var body = Buffer.concat(chunks)
+      var parsed = JSON.parse(body)
+      var ids = parsed.licenses
+        .filter(function (license) {
+          return license.isOsiApproved
+        })
+        .map(function (license) {
+          return license.licenseId
+        })
+        .sort()
+      console.log(JSON.stringify(ids, null, 2))
+    })
+})
